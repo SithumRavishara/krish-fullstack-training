c930503bcc6a21525b68a29fdbf3d39ef9ba1404
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2022-08-10 14:40:01 +0530

    Project Backend added

---
+++ Fuel Distribution System/Backend/src/main/java/com/sithum/FuelDistributionSampleApp/FuelDistributionSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.sithum.FuelDistributionSampleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelDistributionSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelDistributionSampleAppApplication.class, args);
+	}
+
+}
---
+++ Fuel Distribution System/Backend/src/test/java/com/sithum/FuelDistributionSampleApp/FuelDistributionSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sithum.FuelDistributionSampleApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelDistributionSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
